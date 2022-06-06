874d94c570db561cc5523b075388ed50eb988a95
yamada <yamada@xxx.com>
2022-06-07 08:43:26 +0900

    ポートとcorsの設定をenvから読み取るように

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 'use strict'
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.use(express.json())
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.ACCESS_CONTROL_ALLOW_ORIGIN);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 app.use('/api/questions', require('./routes/questions'))
 
-app.listen(3000)
+app.listen(process.env.LISTEN_PORT)
