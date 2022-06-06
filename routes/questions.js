a90d85b2035fbf6f0aa269513900ca55fb3196b8
yamada <yamada@xxx.com>
2022-06-06 19:36:00 +0900

    検索処理追加

--- routes/questions.js
+++ routes/questions.js
@@ -1,35 +1,57 @@
 'use strict'
 const express = require('express')
-const req = require('express/lib/request')
 const { MongoClient } = require('mongodb')
 require('dotenv').config()
 
 const router = express.Router()
 const client = new MongoClient(process.env.MONGODB_URI);
 
-router.route('/').post((req, res) => {
-    run(req).catch(console.dir);
+router.route('/')
+.post((req, res) => {
+    register(req.body).then(() => res.status(200).send('OK'))
+    .catch(console.dir);
+})
+.get((req, res) => {
+    find(req.params).then(data => res.json(data))
 })
 
-async function run(req) {
+async function register(data) {
     try {
         // console.log(req.body)
         await client.connect();
-
         const database = client.db("fullstack");
         const question = database.collection("questions");
 
         const doc = {
-            title: req.body.title,
+            ...data
         }
 
         const result = await question.insertOne(doc);
 
-        console.log(result)
+        // console.log(result)
 
     } finally {
         await client.close();
     }
+}
+
+async function find(params) {
+    await client.connect();
+    const database = client.db("fullstack");
+    const questions = database.collection("questions");
+    const query = {
+        ...params
+    }
+    const options = {
+        sort: { _id: 1},
+        projection: {_id: 1, category: 1, unit: 1, title: 1, type: 1},
+    }
+    
+    const result = questions.find(query, options)
+
+    // console.log(await result.toArray())
+
+    return await result.toArray();
 }
 
 module.exports = router
