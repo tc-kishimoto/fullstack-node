56d2bd7ba6d86addec0e4ae979ab1f9527dec212
yamada <yamada@xxx.com>
2022-06-08 12:47:00 +0900

    corsエラー対応

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 'use strict'
 const express = require('express')
 const app = express()
+const cors = require('cors')
 require('dotenv').config()
 
 app.use(express.json())
+app.use(cors())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.ACCESS_CONTROL_ALLOW_ORIGIN);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use('/api/questions', require('./routes/questions'))
 
