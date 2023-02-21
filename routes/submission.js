c92a204238b945b7f15c5fe9534f277353600d15
ke-kishimoto <nebinosuk@gmail.com>
2023-02-21 22:54:21 +0900

    insertを共通を使用

--- routes/submission.js
+++ routes/submission.js
@@ -1,18 +1,26 @@
 'use strict';
 const express = require('express')
+const common = require('../db/common')
 const { MongoClient } = require('mongodb')
 require('dotenv').config()
 
 const router = express.Router()
 const client = new MongoClient(process.env.MONGODB_URI);
 
+const collectionName = 'submission';
+
 router.route('/')
 .post((req, res) => {
-  register(req.body).then((data) => {
+  common.insertOne(collectionName, req.body)
+  .then((data) => {
     res.json(data)
-    // res.status(200).send('OK')
-})
+  })
   .catch(console.dir);
+})
+
+router.route('/id')
+.put((req, res) => {
+
 })
 
 router.route('/:userId')
@@ -20,33 +28,16 @@ router.route('/:userId')
   findByUserId(req.params.userId).then((data) => {
     res.json(data)
   })
+  .catch(console.dir);
 })
 
-
-// 登録
-async function register(data) {
-  try {
-      await client.connect();
-      const database = client.db("fullstack");
-      const submission = database.collection("submission");
-
-      const now = new Date();
-
-      const doc = {
-        ...data,
-        date: `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`,
-        created_at: `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
-      }
-
-      const result = await submission.insertOne(doc);
-
-      return result;
-  } catch(error) {
-    console.log(error);
-  } finally {
-      await client.close();
-  }
-}
+router.route('/:userId/:category/:lessonType/:lessonName')
+.get((req, res) => {
+  findOne(req.params).then((data) => {
+    res.json(data)
+  })
+  .catch(console.dir);
+})
 
 async function findByUserId(userId) {
   try {
@@ -77,6 +68,40 @@ async function findByUserId(userId) {
     return result;
   } finally {
       await client.close();
+  }
+}
+
+async function findOne(params) {
+  try {
+    await client.connect();
+    const database = client.db("fullstack");
+    const submission = database.collection("submission");
+
+    const query = {
+      user_id: Number(params.userId),
+      category: params.category,
+      lesson_type: params.lessonType,
+      lesson_name: params.lessonName,
+    };
+
+    const options = {
+      projection: {
+        _id: 1, 
+        user_id: 1,
+        category: 1,
+        lesson_type: 1,
+        lesson_name: 1,
+        url: 1,
+        comment: 1,
+      }
+    }
+
+    const result = await submission.findOne(query, options);
+    return result;
+  } catch(error) {
+    console.log(error);
+  } finally {
+    await client.close();
   }
 }
 
