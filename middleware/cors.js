c7800567717ec818da9e7a80fa5035dcf70a2170
ke-kishimoto <nebinosuk@gmail.com>
2023-03-03 10:41:37 +0900

    corsを環境変数から取得するように

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 const corsSetting = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", process.env.ACCESS_CONTROL_ALLOW_ORIGIN);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 }
