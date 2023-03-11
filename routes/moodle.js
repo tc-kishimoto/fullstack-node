5bd9dde24842cfc7122a194e02be153e9186c5f7
ke-kishimoto <nebinosuk@gmail.com>
2023-03-11 23:25:16 +0900

    ユーザー取得のSQL修正

--- routes/moodle.js
+++ routes/moodle.js
@@ -5,7 +5,14 @@ const convertJSON = require('../middleware/convert_bigint')
 
 const router = express.Router()
 
-const select_users_sql = `SELECT * FROM mdl_user`
+const select_users_sql = `SELECT 
+id
+, username as user_name
+, firstname as first_name
+, lastname as last_name
+, DATE_FORMAT(FROM_UNIXTIME(timecreated), '%Y-%m-%d %H:%i:%s') as created_at
+, DATE_FORMAT(FROM_UNIXTIME(lastaccess), '%Y-%m-%d %H:%i:%s') as last_access
+FROM mdl_user`
 
 router.route('/users')
 .get(convertJSON, (req, res) => {
