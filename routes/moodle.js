9b0051b40dfa45bee2d75971f7c09d1e2c29823c
ke-kishimoto <nebinosuk@gmail.com>
2023-03-12 13:46:44 +0900

    ユーザーの受験結果取得処理追加

--- routes/moodle.js
+++ routes/moodle.js
@@ -14,9 +14,52 @@ id
 , DATE_FORMAT(FROM_UNIXTIME(lastaccess), '%Y-%m-%d %H:%i:%s') as last_access
 FROM mdl_user`
 
+const select_quiz_user = `
+SELECT 
+mqa.id
+, mu.id user_id
+, username user_name
+, concat(lastname, firstname) full_name
+, mc.shortname course_name
+, mcs.name section_name
+, mq.name test_name
+, mqa.sumgrades score
+, quiz_avg.avg_score
+, mq.grade max_score
+, DATE_FORMAT(FROM_UNIXTIME(mqa.timefinish), '%Y-%m-%d %H:%i:%s') finish_date
+FROM mdl_user mu 
+JOIN mdl_quiz_attempts mqa
+ON mu.id = mqa.userid
+JOIN mdl_quiz mq 
+ON mqa.quiz = mq.id
+JOIN mdl_course mc 
+ON mq.course = mc.id 
+JOIN mdl_course_modules mcm 
+ON mq.course = mcm.course 
+AND mq.id = mcm.instance
+JOIN mdl_course_sections mcs 
+ON mcm.course = mcs.course 
+AND mcm.section = mcs.id
+JOIN (select quiz, AVG(sumgrades) avg_score from mdl_quiz_attempts group by quiz) quiz_avg
+ON mqa.quiz = quiz_avg.quiz
+WHERE mu.id = ?
+AND mcm.module = 18
+`
+
 router.route('/users')
 .get(convertJSON, (req, res) => {
   moodledb.query(select_users_sql)
+  .then((data) => {
+    res.json(data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+router.route('/test-result/user/:userId')
+.get(convertJSON, (req, res) => {
+  moodledb.query(select_quiz_user,[req.params.userId])
   .then((data) => {
     res.json(data)
   })
