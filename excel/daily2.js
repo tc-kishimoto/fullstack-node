67b6dd4aab369bce7c7ba17198fd372244325e3c
ke-kishimoto <nebinosuk@gmail.com>
2023-03-20 17:52:14 +0900

    スタイルを調整

--- excel/daily2.js
+++ excel/daily2.js
@@ -1,7 +1,6 @@
 'use strict';
 const mongo = require('../db/mongo')
 const xlsxPopulate = require('xlsx-populate');
-const path = require('path');
 
 const scoreInfo = {
   little_test: {
@@ -35,6 +34,18 @@ const borderStyle = {
   }
 }
 
+const themeStyle = {
+  horizontalAlignment: "center", 
+  fill: "C4BD97",
+  border: borderStyle
+}
+
+const headerStyle = {
+  horizontalAlignment: "center", 
+  fill: "DDD9C4",
+  border: borderStyle
+}
+
 const createDailyWorkbook = async (params) => {
 
   const workbook = await xlsxPopulate.fromBlankAsync();
@@ -42,8 +53,8 @@ const createDailyWorkbook = async (params) => {
   // // 日報データ取得
   const dailies = await getDailies(params)
     
+  let sheetCount = 0;
   for (const daily of dailies) {
-    let sheetCount = 0;
 
     // 年月日に分割する
     const [year, month, day] = daily.date.split('-');
@@ -69,12 +80,6 @@ const createDailyWorkbook = async (params) => {
 
     newSheet.cell("B2").value('研修日報')
     newSheet.cell("B2").style('fontSize', '18')
-
-    const headerStyle = {
-      horizontalAlignment: "center", 
-      fill: "DDD9C4",
-      border: borderStyle
-    }
 
     newSheet.cell("B4").value('基本情報')
     newSheet.cell("B5").value('日付')
@@ -129,11 +134,6 @@ const createDailyWorkbook = async (params) => {
     newSheet.column("E").width(35)
     newSheet.column("F").width(35)
 
-    const themeStyle = {
-      horizontalAlignment: "center", 
-      fill: "C4BD97",
-      border: borderStyle
-    }
     newSheet.range("B4:F4").merged(true)
     newSheet.range("B4:F4").style(themeStyle)
     newSheet.range("B8:F8").merged(true)
@@ -174,12 +174,6 @@ const createDailyWorkbook = async (params) => {
     newSheet.row(24).height(16)
     newSheet.row(25).height(64)
 
-    // // set column widths
-    // worksheet.columns.forEach((column, index) => {
-    //   newSheet.getColumn(index + 1).width = column.width;
-    // });
-
-
     // 各データをセット
     // 基本情報
     newSheet.cell('B6').value(daily.date)
@@ -212,7 +206,8 @@ const createDailyWorkbook = async (params) => {
       newSheet.cell('B14').style({
         horizontalAlignment: "center", 
         verticalAlignment: "center",
-        border: borderStyle
+        border: borderStyle,
+        wrapText: true
       })
       newSheet.cell('C14').value(daily.speech_task)
       newSheet.cell('D14').value(daily.speech_notice)
@@ -220,7 +215,8 @@ const createDailyWorkbook = async (params) => {
       newSheet.range('B14:F14').style({
         horizontalAlignment: "left", 
         verticalAlignment: "top",
-        border: borderStyle
+        border: borderStyle,
+        wrapText: true
       })
      
     }
@@ -234,7 +230,8 @@ const createDailyWorkbook = async (params) => {
       newSheet.range('B18:F18').style({
         horizontalAlignment: "center", 
         verticalAlignment: "center",
-        border: borderStyle
+        border: borderStyle,
+        wrapText: true
       })
       newSheet.cell('B20').value(daily.personal_develop_work_content)
       newSheet.cell('C20').value(daily.personal_develop_task)
@@ -242,7 +239,8 @@ const createDailyWorkbook = async (params) => {
       newSheet.range('B20:F20').style({
         horizontalAlignment: "left", 
         verticalAlignment: "top",
-        border: borderStyle
+        border: borderStyle,
+        wrapText: true
       })
     } else if (daily.daily_type === 'team_develop') {
       newSheet.cell('B18').value(daily.team_develop_theme)
@@ -253,7 +251,8 @@ const createDailyWorkbook = async (params) => {
       newSheet.range('B18:F18').style({
         horizontalAlignment: "center", 
         verticalAlignment: "center",
-        border: borderStyle
+        border: borderStyle,
+        wrapText: true
       })
       newSheet.cell('B20').value(daily.team_develop_work_content)
       newSheet.cell('C20').value(daily.team_develop_task)
@@ -261,7 +260,8 @@ const createDailyWorkbook = async (params) => {
       newSheet.range('B20:F20').style({
         horizontalAlignment: "left", 
         verticalAlignment: "top",
-        border: borderStyle
+        border: borderStyle,
+        wrapText: true
       })
     } else {
       // 研修内容
@@ -273,7 +273,8 @@ const createDailyWorkbook = async (params) => {
       newSheet.range('B18:F18').style({
         horizontalAlignment: "left", 
         verticalAlignment: "top",
-        border: borderStyle
+        border: borderStyle,
+        wrapText: true
       })
 
       // テスト結果
@@ -298,14 +299,15 @@ const createDailyWorkbook = async (params) => {
     newSheet.range('B25:F25').style({
       horizontalAlignment: "left", 
       verticalAlignment: "top",
-      border: borderStyle
+      border: borderStyle,
+      wrapText: true
     })
 
     sheetCount++;
   }
 
   // 実績
-  // await createActualResultSheet(workbook, params)
+  await createActualResultSheet(workbook, params)
   
   return workbook
 
@@ -333,7 +335,38 @@ const createNonValueCells = (sheet, areas) => {
 
 // 実績シート
 const createActualResultSheet = async (workbook, params) => {
-  const actualResultSheet = workbook.sheet("実績管理表")
+  const sheet = workbook.addSheet("実績管理表")
+
+  sheet.cell("A1").value('実績管理表')
+  sheet.range("A1:C2").merged(true)
+  sheet.cell("A1").style({
+    fontSize: "22"
+  })
+
+  sheet.cell('A5').value('テスト')
+  sheet.range('A5:F5').merged(true)
+  sheet.range('A5:F5').style(themeStyle)
+
+  sheet.cell('G5').value('演習/課題')
+  sheet.range('G5:J5').merged(true)
+  sheet.range('G5:J5').style(themeStyle)
+
+  sheet.cell('A6').value('実施日')
+  sheet.cell('B6').value('対象単元')
+  sheet.cell('C6').value('取得点数')
+  sheet.cell('D6').value('平均点')
+  sheet.cell('E6').value('合計点')
+  sheet.cell('F6').value('満点')
+  sheet.cell('G6').value('単元')
+  sheet.cell('H6').value('演習・課題')
+  sheet.cell('I6').value('演習名')
+  sheet.cell('J6').value('提出日')
+  sheet.range('A6:J6').style(headerStyle)
+
+  sheet.column("B").width(24)
+  sheet.column("I").width(18)
+
+
   // テスト結果取得
   const testResult = await getTestResult(params)
   // 演習提出状況取得
@@ -343,21 +376,55 @@ const createActualResultSheet = async (workbook, params) => {
   // テストエリア
   let row = startRow;
   testResult.forEach(daily => {
-    actualResultSheet.cell(`A${row}`).value(daily.date)
-    actualResultSheet.cell(`C${row}`).value(daily.score)
-    actualResultSheet.cell(`D${row}`).value(daily.average_score)
-    actualResultSheet.cell(`B${row}`).value (`${daily.test_taraget}(${scoreInfo[daily.test_category].label})`)
-    actualResultSheet.cell(`E${row}`).value(scoreInfo[daily.test_category].passing_score)
-    actualResultSheet.cell(`F${row}`).value(scoreInfo[daily.test_category].max_score)
+    sheet.row(row).height(40)
+    sheet.cell(`A${row}`).value(daily.date)
+    sheet.cell(`C${row}`).value(daily.score)
+    sheet.cell(`D${row}`).value(daily.average_score)
+    sheet.cell(`B${row}`).value (`${daily.test_taraget}(${scoreInfo[daily.test_category].label})`)
+    sheet.cell(`E${row}`).value(scoreInfo[daily.test_category].passing_score)
+    sheet.cell(`F${row}`).value(scoreInfo[daily.test_category].max_score)
+    sheet.range(`A${row}:F${row}`).style({
+      horizontalAlignment: "center", 
+      verticalAlignment: "center",
+      border: borderStyle,
+    })
+    sheet.range(`C${row}:F${row}`).style({
+      fontSize: 14
+    })
     row++;
+  })
+  sheet.range(`F5:F${row-1}`).style({
+    border: {
+      top: {
+        style: 'thin',
+        color: '000000'
+      },
+      left: {
+        style: 'thin',
+        color: '000000'
+      },
+      bottom: {
+        style: 'thin',
+        color: '000000'
+      },
+      right: {
+        style: 'double',
+        color: '000000'
+      }
+    }
   })
   // 演習エリア
   row = startRow
   submissions.forEach(submission => {
-    actualResultSheet.cell(`G${row}`).value(submission.category)
-    actualResultSheet.cell(`H${row}`).value(submission.lesson_type)
-    actualResultSheet.cell(`I${row}`).value(submission.lesson_name)
-    actualResultSheet.cell(`J${row}`).value(submission.date)
+    sheet.cell(`G${row}`).value(submission.category)
+    sheet.cell(`H${row}`).value(submission.lesson_type)
+    sheet.cell(`I${row}`).value(submission.lesson_name)
+    sheet.cell(`J${row}`).value(submission.date)
+    sheet.range(`G${row}:J${row}`).style({
+      horizontalAlignment: "center", 
+      verticalAlignment: "center",
+      border: borderStyle,
+    })
     row++;
   })
 }
