e6fe600bc8239119f25b3e9ca90556c305956cb8
ke-kishimoto <nebinosuk@gmail.com>
2023-03-29 21:45:17 +0900

    エラーにならないように条件分岐追加

--- routes/quizAnswer.js
+++ routes/quizAnswer.js
@@ -39,25 +39,29 @@ router.route('/:id')
   .then(() => {    
     mongo.findById(collectionName, req.params.id)
     .then(data => {
-      const newDetail = data.detail.map(q => {
-        if ((q.type === 'radio' || q.type === 'text') && q.answer === q.user_answer) {
-          return { ...q, score: q.maxScore };
-        }
-        if (q.type === 'checkbox' && q.user_answer !== null) {
-          const answer1 = q.answer.sort();
-          const answer2 = q.user_answer.sort();
-          if(answer1.every((value, index) => value === answer2[index])) {
+      if(data != null) {
+        const newDetail = data.detail.map(q => {
+          if ((q.type === 'radio' || q.type === 'text') && q.answer === q.user_answer) {
             return { ...q, score: q.maxScore };
           }
-        }
-        return q;
-      })
-      data.detail = newDetail;
-      data.score = newDetail.reduce((acc, curr) => acc + curr.score, 0)
-      mongo.updateOne(collectionName, req.params.id, data)
-      .then((data2) => {
-        res.json(data2)
-      })
+          if (q.type === 'checkbox' && q.user_answer !== null) {
+            const answer1 = q.answer.sort();
+            const answer2 = q.user_answer.sort();
+            if(answer1.every((value, index) => value === answer2[index])) {
+              return { ...q, score: q.maxScore };
+            }
+          }
+          return q;
+        })
+        data.detail = newDetail;
+        data.score = newDetail.reduce((acc, curr) => acc + curr.score, 0)
+        mongo.updateOne(collectionName, req.params.id, data)
+        .then((data2) => {
+          res.json(data2)
+        })
+      } else {
+        res.status(404)
+      }
     })
 })
   .catch(console.dir);
