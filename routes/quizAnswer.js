2855f3b3e9b8dc60aeed4400b240aa4a1ce67c1c
ke-kishimoto <nebinosuk@gmail.com>
2023-03-29 10:25:49 +0900

    チェックボックスの採点ロジック修正

--- routes/quizAnswer.js
+++ routes/quizAnswer.js
@@ -18,8 +18,29 @@ router.route('/')
 router.route('/:id')
 .put((req, res) => {
   mongo.updateOne(collectionName, req.params.id, req.body)
-  .then((data) => {    
-    res.json(data)
+  .then(() => {    
+    // console.log(data)
+    mongo.findById(collectionName, req.params.id)
+    .then(data => {
+      // console.log(data)
+      const newDetail = data.detail.map(q => {
+        if ((q.type === 'radio' || q.type === 'text') && q.answer === q.user_answer) {
+          return { ...q, score: q.maxScore };
+        }
+        if (q.type === 'checkbox') {
+          const answer1 = q.answer.sort();
+          const answer2 = q.user_answer.sort();
+          if(answer1.every((value, index) => value === answer2[index])) {
+            return { ...q, score: q.maxScore };
+          }
+        }
+        return q;
+      })
+      data.detail = newDetail;
+      mongo.updateOne(collectionName, req.params.id, data).then((data2) => {
+        res.json(data2)
+      })
+    })
 })
   .catch(console.dir);
 })
